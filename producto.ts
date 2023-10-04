c82db64e9fd688452482357a0dfaf78c5948e0d4
candela-natale <candelanatale78@gmail.com>
2023-10-04 15:09:54 -0300

    Cambios realizamos en producto.ts

--- producto.ts
+++ producto.ts
@@ -1,16 +1,14 @@
-import Proveedor from "./proveedor";
+let nextProductId = 1;
 
 export default class Producto{
   private id : number;
   private nombre : string;
   private precio : number;
-  private proveedores : Proveedor[];
 
-  constructor(id : number, nombre : string, precio : number, proveedores){
-    this.id = id;
+  constructor(nombre : string, precio : number){
+    this.id = nextProductId++;
     this.nombre = nombre;
     this.precio = precio;
-    this.proveedores = [];
   }
 
   getId() : number{
@@ -31,20 +29,5 @@ export default class Producto{
 
   setPrecio(nuevoPrecio : number){
     this.precio = nuevoPrecio;
-  }
-
-  getProveedor() : Proveedor[]{
-    return this.proveedores;
-  }
-  
-  setProveedor(proveedor : Proveedor) : void{
-    this.proveedores.push(proveedor)
-  }
-
-  public eliminarProveedor(proveedor : Proveedor) : void{
-    let index = this.proveedores.indexOf(proveedor);
-    if (index !== -1){
-      this.proveedores.splice(index, 1);
-    }
   }
 }
